11c4c57d7fb13ff5baf284d4d6c2889f4d82e46e
sesychev <sergeysychou@gmail.com>
2024-03-05 11:01:16 +0300

    feat: makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -114,8 +114,18 @@ function makeImmutable(/* obj */) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const arr = [];
+
+  Object.entries(lettersObject).forEach((obj) => {
+    const key = obj[0];
+    const value = obj[1];
+    value.forEach((letter) => {
+      arr[letter] = key;
+    });
+  });
+
+  return arr.join('');
 }
 
 /**
